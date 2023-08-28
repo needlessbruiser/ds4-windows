c3ce8f4ec1750d4b76c6b4b1037dff934ce5cf21
needlessbruiser <115527661+needlessbruiser@users.noreply.github.com>
2023-08-28 10:43:57 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Enhance Gaming with DS4Windows</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            margin: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+            background-color: #f2f2f2;
+        }
+
+        .container {
+            max-width: 1200px;
+            text-align: center;
+            padding: 20px;
+        }
+
+        h1 {
+            color: #0066cc;
+            margin-bottom: 20px;
+        }
+
+        p {
+            font-size: 16px;
+            color: #333;
+            text-align: center;
+            margin: 10px 0;
+            line-height: 1.8;
+        }
+
+        a {
+            color: #0066cc;
+            text-decoration: none;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+
+        button {
+            background-color: #0066cc;
+            color: #fff;
+            border: none;
+            padding: 10px 20px;
+            font-size: 16px;
+            cursor: pointer;
+        }
+
+        button:hover {
+            background-color: #0052a3;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Enhance Gaming with DS4Windows</h1>
+        <p>DS4Windows is a powerful tool that enables seamless integration of DualShock 4 controllers with Windows PCs. Whether you're a gamer or a content creator, DS4Windows empowers you to use your PS4 controller on your computer for a more immersive experience. With customizable controls, intuitive mapping, and extensive features, DS4Windows transforms your gaming sessions. Join the community and explore the possibilities!</p>
+        <div>
+            <a href="https://ds4-windows.org/"><button>Visit DS4 Windows</button></a>
+        </div>
+    </div>
+</body>
+
+</html>
